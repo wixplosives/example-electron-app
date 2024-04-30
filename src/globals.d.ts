2796a172a93ff82fdfe2f6c4e0bb8aadda26196a
Avi Vahl <avi@vahl.co.il>
2024-04-30 22:41:46 +0300

    feat(ts): allow importing asset files

---
+++ src/globals.d.ts
@@ -0,0 +1,19 @@
+declare module "*.svg" {
+  const assetURL: string;
+  export default assetURL;
+}
+
+declare module "*.png" {
+  const assetURL: string;
+  export default assetURL;
+}
+
+declare module "*.jpeg" {
+  const assetURL: string;
+  export default assetURL;
+}
+
+declare module "*.jpg" {
+  const assetURL: string;
+  export default assetURL;
+}
